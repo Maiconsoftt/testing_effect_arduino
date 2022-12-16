8066cf5849070b570e847790433d0870529a946a
Maiconsoftt <brunofelipemuniz@gmail.com>
2022-12-16 13:14:27 -0300

    created hpp declations

--- src/randomizations.hpp
+++ src/randomizations.hpp
@@ -3,8 +3,10 @@
 
 #include <Arduino.h>
 
+void randomize_gate_for_repertory_trainning();
 void Randomize_gates_for_stages ();
 void randomize_pairs_sounds_and_figures();
+void random_seed_repeat ();
 
 
 #endif
