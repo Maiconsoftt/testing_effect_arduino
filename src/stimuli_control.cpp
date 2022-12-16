94bdc8100d62c1a64a877c11a17e265d19dcd145
Maiconsoftt <brunofelipemuniz@gmail.com>
2022-12-16 13:16:04 -0300

    Routines for basal modes and servos

--- src/stimuli_control.cpp
+++ src/stimuli_control.cpp
@@ -19,14 +19,6 @@ void start_light_stimulus(String gate_name, String received_figures){
     }
 }
 
-void start_all_lights_basal_mode (){
-    LedControl led_matrix_controller = get_led_array_by_gate_name("CenterGate");
-    
-    draw_ball(led_matrix_controller, PARAMETER::MAX_INTENSITY_VALUE); 
-    draw_plus(led_matrix_controller, PARAMETER::MAX_INTENSITY_VALUE);
-    draw_X (led_matrix_controller, PARAMETER::MAX_INTENSITY_VALUE);
-}
-
 void start_basal_light_stimulus(String gate_name, String figure){
     LedControl led_matrix_controller = get_led_array_by_gate_name(gate_name);
     
@@ -39,6 +31,16 @@ void start_basal_light_stimulus(String gate_name, String figure){
     else if (figure == "Figure_+"){
         draw_plus (led_matrix_controller, PARAMETER::MIN_INTENSITY_VALUE);
     }
+}
+
+void start_light_1_basal_mode (String gate_name_1, String figure_1){
+    start_basal_light_stimulus (gate_name_1, figure_1); 
+}
+void start_light_2_basal_mode (String gate_name_2, String figure_2){
+    start_basal_light_stimulus (gate_name_2, figure_2); 
+}
+void start_light_3_basal_mode (String gate_name_3, String figure_3){
+    start_basal_light_stimulus (gate_name_3, figure_3); 
 }
 
 
@@ -56,52 +58,69 @@ void start_sound_stimulus(String randomized_sound){
 
 void open_target_gate(String randomized_gate){
     if (randomized_gate == "Left Gate"){
-        Gates::open_left_gate ();
+        Gates::open_gate_by_name (randomized_gate);
     }
        else if (randomized_gate == "CenterGate"){
-        Gates::open_center_gate();
+        Gates::open_gate_by_name (randomized_gate);
     }
         else if (randomized_gate == "Right Gate"){
-        Gates::open_right_gate();
+        Gates::open_gate_by_name (randomized_gate);
     }
 }
 
 void close_target_gate(String randomized_gate){
     if (randomized_gate == "Left Gate"){
-        Gates::close_left_gate ();
+        Gates::close_gate_by_name (randomized_gate);
     }
        else if (randomized_gate == "CenterGate"){
-        Gates::close_center_gate();
+        Gates::close_gate_by_name (randomized_gate);
     }
         else if (randomized_gate == "Right Gate"){
-        Gates::close_right_gate();
+        Gates::close_gate_by_name (randomized_gate);
     }
 }
 
-
 void execute_paired_stimuli(){
-    start_all_lights_basal_mode ();
+    
+    start_light_1_basal_mode (
+        ExperimentConfiguration::get_current_gate(), 
+        ExperimentConfiguration::get_light_symbol()
+    );
 
-    for (int a = 0; a < 5; a++){
-        start_basal_light_stimulus(
-            ExperimentConfiguration::get_current_basal_figure_1(), 
-            ExperimentConfiguration::get_current_basal_figure_1()
-        );
-        start_basal_light_stimulus(
-            ExperimentConfiguration::get_current_basal_figure_2(), 
-            ExperimentConfiguration::get_current_basal_figure_2()
-        );
+    start_basal_light_stimulus(
+        ExperimentConfiguration::get_current_basal_gate_1(), 
+        ExperimentConfiguration::get_current_basal_figure_1()
+    );
+    start_basal_light_stimulus(
+        ExperimentConfiguration::get_current_basal_gate_2(), 
+        ExperimentConfiguration::get_current_basal_figure_2()
+    );
+
+    for (int a = 0; a < 6; a++){
         start_light_stimulus(
             ExperimentConfiguration::get_current_gate(), 
             ExperimentConfiguration::get_light_symbol()
         );
-        start_sound_stimulus(ExperimentConfiguration::get_sound());
+            start_sound_stimulus(ExperimentConfiguration::get_sound());
     }
     delay(5);
 }
 
 void execute_separated_stimuli(){
-    start_all_lights_basal_mode();
+
+    start_light_1_basal_mode (
+        ExperimentConfiguration::get_current_basal_gate_1(), 
+        ExperimentConfiguration::get_current_basal_figure_1()
+    );
+    start_light_2_basal_mode (
+        ExperimentConfiguration::get_current_basal_gate_2(), 
+        ExperimentConfiguration::get_current_basal_figure_2()
+    );
+    start_light_3_basal_mode (
+        ExperimentConfiguration::get_current_gate(), 
+        ExperimentConfiguration::get_light_symbol()
+    );
+
     for (int a = 0; a < 6; a++){
         start_sound_stimulus(ExperimentConfiguration::get_sound());
         delay (10);
