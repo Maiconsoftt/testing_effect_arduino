672d2abffd5cf8a23e792030d1e397f62948f82c
Maiconsoftt <brunofelipemuniz@gmail.com>
2022-12-16 13:16:37 -0300

    Hpp declaration of new functions

--- src/stimuli_control.hpp
+++ src/stimuli_control.hpp
@@ -5,8 +5,9 @@
 
 void start_light_stimulus(String gate_name, String received_figure);
 void start_sound_stimulus(String som_randomizado);
-void start_basal_light_stimulus(String gate_name, String basal_light_1);
-void start_all_lights_basal_mode ();
+void start_basal_light_stimulus(String gate_name, String figure);
+
+void start_target_light_basal_mode (String gate_name_1, String figure_1);
 
 void open_target_gate(String randomized_gate);
 void close_target_gate(String randomized_gate);
@@ -15,5 +16,7 @@ void open_all_gates();
 void execute_paired_stimuli();
 void execute_separated_stimuli();
 void shutdown_all_displays();
+
+
 
 #endif
