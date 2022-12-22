5984c9e6e8aa8dd7f56fa73203a06bf9b6bd0a68
Maiconsoftt <brunofelipemuniz@gmail.com>
2022-12-22 01:18:45 -0300

    Changed all variables to integer

--- src/experiment_configuration.hpp
+++ src/experiment_configuration.hpp
@@ -6,45 +6,52 @@
 class ExperimentConfiguration{
     public: 
         
-        static String current_pair;
-        static String current_gate;
-        static String current_repertory_gate;
-        static String pair_x_sound;
-        static String pair_x_figure;
-        static String pair_y_sound;
-        static String pair_y_figure;
-        static String pair_z_sound;
-        static String pair_z_figure;
-        static String randomized_gate;
-        static String random_repertory_gate;
-        static String current_basal_gate_1;
-        static String current_basal_gate_2;
-        static String current_basal_figure_1;
-        static String current_basal_figure_2;
+        static int current_pair;
+        static int current_gate;
+        static int current_repertory_gate;
+        static int pair_x_sound;
+        static int pair_x_figure;
+        static int pair_y_sound;
+        static int pair_y_figure;
+        static int pair_z_sound;
+        static int pair_z_figure;
+        static int randomized_gate;
+        static int random_repertory_gate;
+        static int current_basal_gate_1;
+        static int current_basal_gate_2;
+        static int current_basal_figure_1;
+        static int current_basal_figure_2;
 
-        static void set_current_pair(String current_pair);
-        static void set_pair_x_sound(String new_pair);
-        static void set_pair_y_sound(String new_pair);
-        static void set_pair_z_sound(String new_pair);
-        static void set_pair_x_figure(String new_pair);
-        static void set_pair_y_figure(String new_pair);
-        static void set_pair_z_figure(String new_pair);
-        static void set_current_gate(String gate);
-        static void set_current_repertory_gate(String repertory_gate);
-        static void set_current_basal_gate_1(String basal_gate_1);
-        static void set_current_basal_gate_2(String basal_gate_2);
-        static void set_current_basal_figure_1(String basal_figure_2);
-        static void set_current_basal_figure_2(String basal_figure_2);
+        static void set_current_pair(int current_pair);
+        static void set_pair_x_sound(int x_sound);
+        static void set_pair_y_sound(int y_sound);
+        static void set_pair_z_sound(int z_sound);
+        static void set_pair_x_figure(int x_figure);
+        static void set_pair_y_figure(int y_figure);
+        static void set_pair_z_figure(int z_figure);
+        static void set_current_gate(int current_gate);
+        static void set_current_repertory_gate(int current_repertory_gate);
+        static void set_current_basal_gate_1(int basal_gate_1);
+        static void set_current_basal_gate_2(int basal_gate_2);
+        static void set_current_basal_figure_1(int basal_figure_2);
+        static void set_current_basal_figure_2(int basal_figure_2);
         
         
-        static String get_light_symbol();
-        static String get_sound();
-        static String get_current_gate();
-        static String get_current_repertory_gate();
-        static String get_current_basal_gate_1();
-        static String get_current_basal_gate_2();
-        static String get_current_basal_figure_1();
-        static String get_current_basal_figure_2();
+        static int get_light_symbol();
+        static int get_sound();
+        static int get_current_pair();
+        static int get_current_gate();
+        static int get_pair_x_figure();
+        static int get_pair_y_figure();
+        static int get_pair_z_figure();
+        static int get_pair_x_sound();
+        static int get_pair_y_sound();
+        static int get_pair_z_sound();
+        static int get_current_repertory_gate();
+        static int get_current_basal_gate_1();
+        static int get_current_basal_gate_2();
+        static int get_current_basal_figure_1();
+        static int get_current_basal_figure_2();
 };
 
 #endif
