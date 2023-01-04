b37d2d6074cf5b8d08a3758fa8a8f0ad08514dfc
Maiconsoftt <brunofelipemuniz@gmail.com>
2023-01-04 17:17:11 -0300

    Reduced Strings lengths

--- src/serial_prints.cpp
+++ src/serial_prints.cpp
@@ -18,13 +18,13 @@ String converting_int_pair_figure_to_string(int randomized_figure){
     String string_figure = "";
     switch (randomized_figure){
         case 0:
-            string_figure = "X_figure";
+            string_figure = "X_figure(0)";
             break;
         case 1:
-            string_figure = "O_figure";
+            string_figure = "O_figure(1)";
             break;
         case 2:
-            string_figure = "+_figure";
+            string_figure = "+_figure(2)"; //27bytes
             break;       
     }
     return string_figure;
@@ -35,13 +35,13 @@ String converting_int_pair_sound_to_string(int randomized_sound){
     String string_sound;
     switch (randomized_sound){
         case 0:
-            string_sound = "100 Hz";
+            string_sound = "100Hz(0)";
             break;
         case 1:
-            string_sound = "750 Hz";
+            string_sound = "750Hz(1)";
             break;
         case 2:
-            string_sound = "2000 Hz";
+            string_sound = "2000Hz(2)"; //22 bytes
             break;       
     }
     return string_sound;
@@ -64,25 +64,25 @@ String converting_int_gate_to_string(int randomized_gate){
 }
 
 void commands () { 
-  Serial.println ("Type 'mouseID' to set mouse indentification and pairs");
-  Serial.println ("Type 'manual' to configure mouse pairs");
-  Serial.println ("Type 'stage + number' to initialize stages (eg: 'stage0')");
-  Serial.println ("Type 'EndStage' to finalize any stage");
-  Serial.println ("Type 'repeat' to repeat Repertory Trainning");
-  Serial.println ("Type 'end' to finish Repertory Tranning"); 
-  Serial.println ("Type 'commands' to see this list again\n");
+    Serial.println ("Type 'mouseID' to set mouse identification and pairs");
+    Serial.println ("Type 'manual' to configure mouse pairs");
+    Serial.println ("Type 'stage + number' to initialize stages (eg: 'stage0')");   
+    Serial.println ("Type 'repeat' to repeat Repertory Training");
+    Serial.println ("Type 'end' to finish Repertory Training"); 
+    Serial.println ("Type 'EndStTrial' to finalize stage trial");  
+    Serial.println ("Type 'EndStAttempt' to finalize any stage");
+    Serial.println ("Type 'commands' to see this list again\n");
 }
 
-void print_repertory_training_counting(){
+void print_counting(int repertory_counting, String seconds){
     char buff [35];
-    Serial.println ("Put the mouse at the central compartment"); delay (1000);
-    Serial.println ("Initializing Repertory Trainning in 60 seconds");
-    for (int repertory_counting = 1; repertory_counting <= 5; repertory_counting ++) {
-        sprintf (buff, "Counting: %ds ...", repertory_counting);
+    Serial.println ("Place the mouse at the central compartment"); delay (1000);
+    Serial.println ("Initializing in " + seconds); delay (1000);
+    for (int counting = 1; counting <= repertory_counting; counting ++) {
+        sprintf (buff, "Counting: %ds ...", counting);
         Serial.println (buff);
         delay (1000);
     }
-    Serial.println ("Beginning Repertoy Training. Randomizing arm to open ..."); delay (1000);
 }
 
 void converting_all_int_pairs_figures_to_string(){
@@ -105,23 +105,23 @@ void converting_all_int_gates_to_string(){
 void print_current_pairs(){
     char message[32];
     if (ExperimentConfiguration::get_current_pair() == 0){
-        sprintf(message, "PairX: %s and %s", pair_x_figure.c_str(), pair_x_sound.c_str());
+        sprintf(message, "PairX: %s & %s", pair_x_figure.c_str(), pair_x_sound.c_str());
     }
     else if (ExperimentConfiguration::get_current_pair() == 1){
-        sprintf(message, "PairY: %s and %s", pair_y_figure.c_str(), pair_y_sound.c_str());
+        sprintf(message, "PairY: %s & %s", pair_y_figure.c_str(), pair_y_sound.c_str());
     }
     else if (ExperimentConfiguration::get_current_pair() == 2){
-        sprintf(message, "PairZ %s and %s", pair_z_figure.c_str(), pair_z_sound.c_str());
+        sprintf(message, "PairZ: %s & %s", pair_z_figure.c_str(), pair_z_sound.c_str());
     }
     Serial.println(message);
 }
 
 void print_all_configurations(){
-    char message[100];
+    char message[150];
     sprintf(
         message, 
-        "Pairs for mouse %s\nX= %s and %s\nY= %s and %s\nZ= %s and %s",
-        ExperimentConfiguration::get_mouse_id().c_str(), 
+        "Mouse %d: \nX= %s & %s\nY= %s & %s\nZ= %s & %s",
+        ExperimentConfiguration::get_mouse_id(), 
         pair_x_figure.c_str(), 
         pair_x_sound.c_str(),
         pair_y_figure.c_str(), 
@@ -134,34 +134,31 @@ void print_all_configurations(){
 
 void print_set_figures_and_sounds (String number){
     char pair [30];
-    sprintf(pair, "Type %s number", number.c_str());
+    sprintf(pair, "Type %s ID", number.c_str());
     Serial.println(pair);
 }
 
-void print_repertory_trainning(){
-    Serial.println ("Oppening: " + current_repertory_gate);
+void print_repertory_training(){
+    Serial.println ("Opening: " + current_repertory_gate);
 }
 
 void print_close_gate(){
     Serial.println ("Closing gate");
 }
 
-void print_current_gate (){
-    Serial.println ("Target gate = " + current_gate);
-}
-
-void print_wait (String time){
-    Serial.println ("Wait " + time);
-}
-
-void print_end_close_gate(){
-    Serial.println ("The gate was closed!");
-}
-
-void print_mouse_message(){
-    Serial.println("Type Mouse Identification Number");
-}
-
-void print_received_serial_input (String input){
-    Serial.println(input);
+void print_received_input(String input){
+    if (input == "60s" || input == "40s" || input == "5s"){
+        Serial.println ("Wait " + input);
+    }
+    else if (input == "mouseID\r\n" || input == "manual\r\n"){
+        Serial.println("Type Mouse ID");
+    }
+    else if (input ==  converting_int_gate_to_string(ExperimentConfiguration::get_current_gate()) 
+    || input == converting_int_gate_to_string(ExperimentConfiguration::get_current_repertory_gate())){
+        Serial.println ("Target gate = " + input);
+    }    
+    else{
+        Serial.println(input);
+    }
+    
 }
--- src/serial_prints.hpp
+++ src/serial_prints.hpp
@@ -5,7 +5,6 @@
 
 void commands();
 
-void print_repertory_training_counting();
 void print_current_pairs ();
 
 void converting_all_int_pairs_figures_to_string();
@@ -14,12 +13,10 @@ void converting_all_int_gates_to_string();
 
 void print_all_configurations();
 void print_set_figures_and_sounds (String number);
-void print_repertory_trainning ();
+void print_repertory_training ();
 void print_close_gate();
-void print_current_gate();
-void print_wait (String time);
-void print_end_close_gate ();
-void print_mouse_message();
-void print_received_serial_input(String input);
+
+void print_counting(int repertory_counting, String seconds);
+void print_received_input(String input);
 
 #endif
